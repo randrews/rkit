9c86f221e1a6bfbcff98160bf259bf25f6e3a86c
Ross Andrews <randrews@geekfu.org>
2010-10-03 01:06:57 -0500

    Test of writing chars to window

--- main.c
+++ main.c
@@ -3,22 +3,20 @@
 #include <string.h>
 #include <time.h>
 
-#include <ncurses.h>
-
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
+
+#include <allegro.h>
 
 #include "map.h"
 
 const int dest_x = 0;
 const int dest_y = 0;
 
-void draw_border(int x, int y, int w, int h);
-void draw_layout();
+BITMAP *font_bmp;
+
 void draw_map(Map* map, int src_x, int src_y, int w, int h);
-void fill_map(Map* map);
-int adjacent(Map* map, int x, int y, char c);
 
 int main(int argc, char** argv){
   char buff[256];
@@ -27,94 +25,34 @@ int main(int argc, char** argv){
   luaL_openlibs(L);
   luaopen_map(L);
   set_draw(&draw_map);
-  set_getkey(&getch);
+  set_getkey(&readkey);
 
-  initscr();
+  allegro_init();
+  install_keyboard();
+  set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0);  
+
+  font_bmp = load_bitmap("Tahin-font.tga", NULL);
 
   char* code = "require('cave')";
   error = luaL_loadbuffer(L, code, strlen(code), "line") || lua_pcall(L, 0, 0, 0);
 
   if(error){
-    mvaddstr(LINES-2, 0, lua_tostring(L,-1));
+    printf("%s\n", lua_tostring(L, -1));
     lua_pop(L, 1);
-    getch();
   }
 
-  endwin();
+  readkey();
   lua_close(L);
+
+  if(font_bmp){destroy_bitmap(font_bmp);}
 
   return 0;
 }
+END_OF_MAIN()
 
 void draw_map(Map* map, int src_x, int src_y, int w, int h){
-  if(!w){w = COLS;}
-  if(!h){h = LINES;}
-
-  int x, y;
-  int src_w = map->w;
-
-  for(y=0; y < h; y++){
-    for(x=0; x < w; x++){
-      char c = map->data[((y+src_y)*src_w) + (x+src_x)%src_w];
-      mvaddch(y, x, c);
-    }
-  }
-}
-
-void fill_map(Map* map){
-  memset(map->data, '.', map->w * map->h);
-
-  srand(time(0));
-
-  int n;
-  for(n=0; n < map->w * map->h; n++){
-    if(rand()%2){continue;}
-    map->data[n] = '+';
-  }
-
-  for(n=0; n<4; n++){
-    int x,y;
-
-    for(y=0; y < map->h; y++){
-      for(x=0; x < map->w; x++){
-	char c = map->data[x + map->w * y];
-	int a = adjacent(map, x, y, '+');
-
-	if(c == '.' && a > 4){ map->data[x + map->w * y] = '+'; }
-	if(c == '+' && a < 4){ map->data[x + map->w * y] = '.'; }
-      }
-    }
-  }
-}
-
-void draw_layout(){
-  int n;
-  for(n = 0; n < COLS; n++){
-    mvaddch(LINES-8, n, 196);
-  }
-
-  for(n = 0; n < LINES-8; n++){
-    mvaddch(n, COLS-21, 179);
-  }
-
-  mvaddch(LINES-8, COLS-21, 193);
-}
-
-void draw_border(int x, int y, int w, int h){
-  mvaddch(y+h,x+w,217);
-  mvaddch(y,x,218);
-  mvaddch(y,x+w,191);
-  mvaddch(y+h,x,192);
-
-  int n;
-
-  for(n=y+1; n < y+h; n++){
-    mvaddch(y+n, x, 179);
-    mvaddch(y+n, x+w, 179);
-  }
-
-  for(n=x+1; n < x+w; n++){
-    mvaddch(y, x+n, 196);
-    mvaddch(y+h, x+n, 196);
-  }
+  BITMAP* letter = create_sub_bitmap(font_bmp, 16, 16, 16, 16);
+  draw_sprite(screen, letter, 80, 50);
+  draw_character_ex(screen, letter, 50, 50, makecol(255, 0, 0), makecol(0, 0, 255));
+  destroy_bitmap(letter);
 }
